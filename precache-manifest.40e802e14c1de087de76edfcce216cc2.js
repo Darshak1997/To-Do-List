74ec2fa1cb2101ae9c04166fa853daff4e60d4e9
Darshak1997 <42878805+Darshak1997@users.noreply.github.com>
2020-06-30 18:58:04 -0500

    Updates

---
+++ precache-manifest.40e802e14c1de087de76edfcce216cc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6b9f11ea692ca49c53b8e3a6d4e7e3",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "3420a87110be87b7306c",
+    "url": "/To-Do-List/static/css/main.034932e2.chunk.css"
+  },
+  {
+    "revision": "a5603e875d3720737575",
+    "url": "/To-Do-List/static/js/2.fb32338a.chunk.js"
+  },
+  {
+    "revision": "3420a87110be87b7306c",
+    "url": "/To-Do-List/static/js/main.1d982f93.chunk.js"
+  },
+  {
+    "revision": "1e5b64d60343e6eba72b",
+    "url": "/To-Do-List/static/js/runtime~main.2fdc73a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-List/static/media/logo.5d5d9eef.svg"
+  }
+]);
